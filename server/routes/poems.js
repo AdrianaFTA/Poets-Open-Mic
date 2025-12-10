0f4ef2210463d7fb33a2cc3c63e48b31254499dd
AdrianaFTA <116352942+AdrianaFTA@users.noreply.github.com>
2025-12-10 18:54:35 +0000

    commit

--- server/routes/poems.js
+++ server/routes/poems.js
@@ -8,19 +8,30 @@ router.post("/", authenticateToken, async (req, res) =>{
     try{
         const {title, content} = req.body;
 
-        const result = await db.query(`INSERT INTO poems(title, content, user_id) VALUES($1, $2, $3) RETURNING *`, [title, content, req.user.id]);
+        //input validation
+        if (!title || !content){
+            return res.status(400).json({message: "Title and context required for a poem"});
 
-        res.json(result.rows[0]);
-    } catch  (err) {
+        }
+        const result = await db.query(
+            `INSERT INTO poems(title, content, user_id) VALUES($1, $2, $3) RETURNING*`, [title, content, req.user.id]
+        );
+        res.status(201).json(result.rows[0]);
+    }catch (err) {
         console.error("Create Poem ERROR:", err);
-        res.status(500).json({ message: "Error when saving poem"});
+        res.status(500).json({message: "Error when saving poem"});
     }
-    });
+});
 
-    // get poems 
-    router.get("/", async (req, res) =>{
+// get poems
+router.get("/", async (req, res) => {
+    try {
         const poems = await db.query("SELECT * FROM poems ORDER BY id DESC");
         res.json(poems.rows);
-    });
-
-    module.exports = router;
+    }catch (err) {
+        console.error("Get Poems ERROR:", err);
+        res.status(500).json({message: "Error when fetching poems"});
+    
+    }
+});
+module.exports = router; 
