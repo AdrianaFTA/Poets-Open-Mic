8c50b4caeb6f69c52964b02bc8c7be0aaca7b34e
AdrianaFTA <116352942+AdrianaFTA@users.noreply.github.com>
2025-12-10 15:06:07 +0000

    add postgresSQL connection pool logic

--- server/db.js
+++ server/db.js
@@ -1,9 +1,20 @@
 const { Pool } = require("pg");
+if(!process.env.PG_USER || !process.env.PG_HOST || !process.env.PG_DATABASE || !process.env.PG_PORT){
+    console.error("FATAL ERROR: Database enviroment variables (PG_USER, PG_HOST, etc.) are missing");
+}
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://postgres:password@localhost:5432/poetsdb"
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  
 });
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+pool.on('connect', () => {
+    console.log('Database connected successfully');
+});
+pool.on('error', (err, client) =>{
+    console.error('Unexpected error on idle client', err);
+});
+module.exports = pool;
