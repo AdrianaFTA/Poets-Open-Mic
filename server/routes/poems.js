7da9d89bdd673287fd4b143b84e87c7d6dd85651
AdrianaFTA <116352942+AdrianaFTA@users.noreply.github.com>
2025-12-10 12:52:36 +0000

    commit
    trying to cretae server

---
+++ server/routes/poems.js
@@ -0,0 +1,26 @@
+const express = require("expresss");
+const router = express.Router();
+const authenticateToken = require("./auth");
+const db = require("../db");
+
+//create poem
+router.post("/", authenticateToken, async (req, res) =>{
+    try{
+        const {title, content} = req.body;
+
+        const result = await db.query(`INSERT INTO poems(title, content, user_id) VALUES($1, $2, $3) RETURNING *`, [title, content, req.user.id]);
+
+        res.json(result.rows[0]);
+    } catch  (err) {
+        console.error("Create Poem ERROR:", err);
+        res.status(500).json({ message: "Error when saving poem"});
+    }
+    });
+
+    // get poems 
+    router.get("/", async (req, res) =>{
+        const poems = await db.query("SELECT * FROM poems ORDER BY id DESC");
+        res.json(poems.rows);
+    });
+
+    module.exports = router;
